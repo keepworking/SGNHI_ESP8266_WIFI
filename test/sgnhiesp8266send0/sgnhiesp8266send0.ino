542a51bcf6cd3348ed819e74064d6e9592623029
yevgeny hong <yev_science@naver.com>
2016-08-29 10:44:01 +0900

    Update sgnhiesp8266send0.ino

--- test/sgnhiesp8266send0/sgnhiesp8266send0.ino
+++ test/sgnhiesp8266send0/sgnhiesp8266send0.ino
@@ -1,14 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <SGNHI_ESP8266_WIFI.h>
 
-dotori PIRsensor("qvIic");
+dotori sensor("SENSOR CODE");//센서 객체를 선언합니다.
 
 void setup() {
-  dev.init("admin","1HM3CJsHd1MsiAyi176I");
+  dev.init("USER ID","DEVICE CODE");//장치의 사용을 설정합니다.
 }
 
 void loop() {
+  sensor.set(0);//센서 객체의 값을 0으로 지정합니다.
   WiFi.waitForConnectResult();
-  dev.send(PIRsensor);
+  dev.send(sensor);//센서데이터를 전송합니다.
   delay(1000);
 }
